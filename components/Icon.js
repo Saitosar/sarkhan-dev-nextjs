0800dc3510107a712090b934d0efd63432f5339f
Saitosar <sarkhan.r.hajiyev@gmail.com>
2025-09-10 21:02:27 +0000

    redesign of like button

--- components/Icon.js
+++ components/Icon.js
@@ -65,6 +65,9 @@ const icons = {
       <path className="moon" fill="currentColor" d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
     </>
   ),
+  heart: (
+    <path fill="currentColor" stroke="currentColor" strokeWidth="1" d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
+  ),
   'ai-sparkle': (
     // Финальная версия: "Цифровая Сфера"
     <>
